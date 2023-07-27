b94c454c5cc62825a07aa58e30e93d6e9b4d6cfa
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-07-28 01:48:33 +0600

    feat: :sparkles: ui update

--- src/components/games/thermometer/Thermometer.tsx
+++ src/components/games/thermometer/Thermometer.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Thermoc } from './boardData'
 import { ThermoSVG } from './ThermoSVG'
+// import html2canvas from 'html2canvas';
 
-type Props = {
-
-}
+type Props = {}
 
 export const Thermometer = (props: Props) => {
 
@@ -13,21 +12,26 @@ export const Thermometer = (props: Props) => {
     const [shows, setShows] = React.useState<boolean>(false)
     const [limit, setLimit] = React.useState<number>(1)
 
+    // create sample 
     async function create() {
         let result = new Thermoc()
         let data = result.makeData()
         setTemps(data)
     }
+
+    // create board 
     async function createMulti() {
         let result = new Thermoc()
         let multidata = result.makeMultiData(limit)
         setBoards(multidata)
     }
-    async function handleDownload() {
 
+    // download 
+    async function handleDownload() {
 
         for (let i = 1; i <= limit; i++) {
             let canvasId = `canvas${i}`
+
             // console.log(i)
             const canvas = document.getElementById(canvasId) as HTMLDivElement;
             const svgData = new XMLSerializer().serializeToString(canvas);
@@ -41,7 +45,24 @@ export const Thermometer = (props: Props) => {
             link.click();
             document.body.removeChild(link);
             URL.revokeObjectURL(url);
+
         }
+
+        // const canvas = document.getElementById(canvasId) as HTMLDivElement;
+
+        // // Convert the HTML/SVG content to a canvas
+        // const canvasElement = await html2canvas(canvas, { scale: 2 });
+
+        // // Convert the canvas to a data URL in PNG format
+        // const dataUrl = canvasElement.toDataURL('image/png');
+
+        // // Create a temporary anchor element to trigger the download
+        // const link = document.createElement('a');
+        // link.href = dataUrl;
+        // link.download = 'canvas.png';
+        // document.body.appendChild(link);
+        // link.click();
+        // document.body.removeChild(link);
     }
 
     return (
@@ -51,13 +72,25 @@ export const Thermometer = (props: Props) => {
                 <div className='col-span-5'>
                     <h1 className='px-4 py-2 text-xl text-pink-600'>Thermometer</h1>
                 </div>
-                <div className='grid col-span-5 lg:col-span-3 p-4 bg-pink-50 gap-4 rounded-lg shadow-lg'>
+                <div className='grid col-span-5 lg:col-span-3 p-4 justify-center bg-pink-50 gap-4 rounded-lg shadow-lg'>
                     <div className='max-h-80'>
                         <ThermoSVG temps={temps} shows={shows} />
                     </div>
-                    {temps.temp1 !== undefined && (
-                        <button onClick={() => setShows(!shows)} type='button'>
-                            {shows ? "Hide Answer" : "Show Answer"}
+                    {temps?.temp1 !== undefined && (
+                        <button onClick={() => setShows(!shows)} type='button' className='px-2 py-1 shadow-lg hover:bg-pink-600 hover:text-white bg-pink-200 justify-self-center rounded-md w-24 text-center flex justify-center text-pink-600'>
+                            {shows ? (
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                                    <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                                    <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                                </svg>
+
+                            ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                    <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                                </svg>
+                            )}
                         </button>
                     )}
                 </div>
@@ -93,9 +126,9 @@ export const Thermometer = (props: Props) => {
                     </div>
                 </div>
 
-                <div className='col-span-5 px-4 py-10 flex flex-wrap gap-4 justify-center bg-pink-50 rounded-lg w-full'>
+                <div className='col-span-5 md:px-0 px-4 py-10 flex flex-wrap gap-4 justify-center bg-pink-50 rounded-lg w-full'>
                     {boards?.map((x: any, index: number) => (
-                        <div id={`canvas${index + 1}`} key={index} className='max-h-[45vh] w-[45vh] rounded-md bg-pink-200 shadow-lg'>
+                        <div id={`canvas${index + 1}`} key={index} className='max-h-[45vh] w-full flex justify-center items-center rounded-md bg-pink-200 shadow-lg'>
                             <ThermoSVG temps={x} shows={shows} />
                         </div>
                     ))}
--- src/pages/games/[slug].tsx
+++ src/pages/games/[slug].tsx
@@ -12,7 +12,6 @@ const GamePage = (props: Props) => {
   return (
     <>
       <Thermometer />
-      
     </>
   )
 }
